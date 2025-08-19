c9c8ed07c3e96c89251abdf0defd01255cb9df5c
Mehakpreet Singh <159048970+OfficialMehak@users.noreply.github.com>
2025-08-19 21:09:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# Assignment 1 – Section B
+# Write your answers below each question
+
+# Q11: Swap variables
+# Your code here
+
+# Q12: Number pattern
+# Your code here
+
+# Q13: Factorial OR Password System
+# Your code here
